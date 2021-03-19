1c605d1fb1acc2c5f8cd81881ad58a0fabd86ef5
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-03-18 20:44:51 -0400

    Start doing actual coding

--- src/main/java/tk/kulzers/fabric/moretotems/MoreTotems.java
+++ src/main/java/tk/kulzers/fabric/moretotems/MoreTotems.java
@@ -1,8 +1,19 @@
 package tk.kulzers.fabric.moretotems;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.*;
+import net.minecraft.util.Identifier;
 
 public class MoreTotems implements ModInitializer {
+	public static final Item ROTTEN_TOTEM = new Item(new FabricItemSettings().maxCount(1));
+	public static final ItemGroup GROUP = FabricItemGroupBuilder.create(new Identifier("moretotems", "group"))
+			.icon(() -> new ItemStack(ROTTEN_TOTEM))
+			.appendItems(stacks -> {
+				stacks.add(new ItemStack(ROTTEN_TOTEM));
+			})
+			.build();
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
